8ae08835f5da251776982eb3d81141c61945dd86
지농 <jinhong0719@naver.com>
2021-04-15 11:21:34 +0900

    [ADD] <auth/repository/mysql/auth_repo.go> Add mysqlAuthRepository

--- auth/repository/mysql/auth_repo.go
+++ auth/repository/mysql/auth_repo.go
@@ -1,2 +1,23 @@
 package mysql
 
+import (
+	"database/sql"
+	"github.com/pkg/errors"
+	"log"
+
+	"github.com/MyFirstBabyTime/Server/domain"
+)
+
+// mysqlAuthRepository is implementation of domain.AuthRepository using mysql
+type mysqlAuthRepository struct {
+	conn *sql.DB
+}
+
+// NewMysqlAuthRepository return implementation of domain.AuthRepository using mysql
+func NewMysqlAuthRepository(conn *sql.DB) domain.AuthRepository {
+	repo := &mysqlAuthRepository{conn}
+	if err := repo.migrateSchema(domain.AuthMysqlSchema); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to migrate schema").Error())
+	}
+	return repo
+}
