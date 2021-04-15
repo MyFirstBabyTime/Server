910e4fa8ec8b8fbf15a13cc487a94ca834563f28
지농 <jinhong0719@naver.com>
2021-04-15 11:19:07 +0900

    [ADD] <domain/auth.go> Add ParentAuth model as struct

--- domain/auth.go
+++ domain/auth.go
@@ -1,1 +1,9 @@
 package domain
+
+
+// ParentAuth is model represent parent auth using in auth domain
+type ParentAuth struct {
+	UUID string `json:"uuid" validate:"required"`
+	ID   string `json:"id" validate:"required"`
+	PW   string `json:"pw" validate:"required"`
+}
