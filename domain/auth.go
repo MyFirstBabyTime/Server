473967d2f4930776e15f180ec9846a46f1ce51d9
지농 <jinhong0719@naver.com>
2021-04-15 11:19:38 +0900

    [ADD] <domain/auth.go> Add auth repository abstact as interface

--- domain/auth.go
+++ domain/auth.go
@@ -12,3 +12,16 @@ type ParentAuth struct {
 type AuthUsecase interface {
 	SignUpParent(ctx gin.Context)
 }
+
+// AuthRepository is abstract interface about repository layer using in usecase layer
+type AuthRepository interface {
+	parentAuthRepository
+
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error) // BeginTx method start transaction
+	Commit(tx *sql.Tx) (err error)                                     // Commit method commit transaction
+	Rollback(tx *sql.Tx) (err error)                                   // Rollback method rollback transaction
+}
+
+type parentAuthRepository interface {
+	CreateParentAuth(tx *sql.Tx, auth *ParentAuth) error
+}
