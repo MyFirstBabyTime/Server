b48d97a2aa3e51c73066300d366df3dc5bab970b
지농 <jinhong0719@naver.com>
2021-04-15 22:29:07 +0900

    [MODIFY] <auth/repository/mysql/auth_repo.go> Change migrate method

--- auth/repository/mysql/auth_repo.go
+++ auth/repository/mysql/auth_repo.go
@@ -3,6 +3,10 @@ package mysql
 import (
 	"context"
 	"database/sql"
+	"github.com/Masterminds/squirrel"
+	"github.com/VividCortex/mysqlerr"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"log"
 
@@ -32,8 +36,26 @@ func (ar *mysqlAuthRepository) Commit(tx *sqlx.Tx) error { return tx.Commit() }
 // Rollback is method of domain.AuthRepository interface
 func (ar *mysqlAuthRepository) Rollback(tx *sqlx.Tx) error { return tx.Rollback() }
 
-// migrate
-func (ar *mysqlAuthRepository) migrateSchema(schema string) (err error) {
-	_, err = ar.conn.Exec(schema)
+// migrateModel method migrate model received from parameter to this repository
+func (ar *mysqlAuthRepository) migrateModel(model interface {
+	TableName() string // TableName return table name about model
+	Schema() string    // Schema return schema SQL about model
+}) (err error) {
+	_sql, _, _ := squirrel.Select("*").From(model.TableName()).ToSql()
+	switch _, err = ar.db.Query(_sql); tErr := err.(type) {
+	case nil:
+		break
+	case *mysql.MySQLError:
+		switch tErr.Number {
+		case mysqlerr.ER_NO_SUCH_TABLE:
+			_, err = ar.db.Exec(model.Schema())
+			err = errors.Wrapf(err, "failed to exec %s model schema", model.TableName())
+		default:
+			err = errors.Wrapf(err, "check table query returns unexpected mysql error code")
+		}
+	default:
+		err = errors.Wrapf(err, "check table query returns unexpected error type")
+	}
+
 	return
 }
