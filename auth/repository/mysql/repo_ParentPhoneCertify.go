36770f26f36cffd77e8abdcb1c87579ec18f802a
지농 <jinhong0719@naver.com>
2021-04-16 20:13:28 +0900

    [ADD] Implement GetByPhoneNumber method

--- auth/repository/mysql/repo_ParentPhoneCertify.go
+++ auth/repository/mysql/repo_ParentPhoneCertify.go
@@ -1,6 +1,9 @@
 package mysql
 
 import (
+	"database/sql"
+	"github.com/Masterminds/squirrel"
+	"github.com/MyFirstBabyTime/Server/tx"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"log"
@@ -10,6 +13,8 @@ import (
 
 // parentPhoneCertifyRepository is implementation of domain.AuthRepository using mysql
 type parentPhoneCertifyRepository struct {
+	domain.ParentPhoneCertifyRepository
+
 	db           *sqlx.DB
 	migrator     migrator
 	sqlMsgParser sqlMsgParser
@@ -25,4 +30,19 @@ func ParentPhoneCertifyRepository(db *sqlx.DB, sp sqlMsgParser) domain.ParentPho
 		log.Fatal(errors.Wrap(err, "failed to migrate parent phone certify").Error())
 	}
 	return repo
+}
+
+func (pp *parentPhoneCertifyRepository) GetByPhoneNumber(ctx tx.Context, pn string) (certify domain.ParentPhoneCertify, err error) {
+	_tx, _ := ctx.Tx().(*sqlx.Tx)
+	_sql, args, _ := squirrel.Select("*").From("parent_phone_certify").Where("phone_number = ?", pn).ToSql()
+
+	switch err = _tx.Get(&certify, _sql, args...); err {
+	case nil:
+		break
+	case sql.ErrNoRows:
+		err = rowNotExistErr{errors.Wrap(err, "failed to select parent phone certify")}
+	default:
+		err = errors.Wrap(err, "select parent phone certify return unexpected error")
+	}
+	return
 }
