0c5dafa3d2c7b2966b73d07521502da76b570834
지농 <jinhong0719@naver.com>
2021-04-16 16:26:04 +0900

    [ADD] <auth/repository/mysql/error.go> Add isRowNotExist function

--- auth/repository/mysql/error.go
+++ auth/repository/mysql/error.go
@@ -6,3 +6,13 @@ type rowNotExistErr struct {
 }
 func (err rowNotExistErr) Error() string { return err.error.Error() }
 func (err rowNotExistErr) RowNotExist() bool { return true }
+
+// isRowNotExist method return if err is row not exist
+func isRowNotExist(err error) bool {
+	type rowNotExist interface {
+		RowNotExist() bool
+	}
+
+	re, ok := err.(rowNotExistErr)
+	return ok && re.RowNotExist()
+}
