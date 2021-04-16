ec21b3c05500ceb735bd3fcd57387fa828aec164
지농 <jinhong0719@naver.com>
2021-04-16 11:40:30 +0900

    [ADD] <tx/handler_sqlx.go> Add sqlxHandler struct

--- tx/handler_sqlx.go
+++ tx/handler_sqlx.go
@@ -1,1 +1,9 @@
 package tx
+
+import (
+	"database/sql"
+)
+
+// sqlxHandler is struct that handle transaction with sqlx package
+type sqlxHandler struct { db *sqlx.DB }
+func NewSqlxHandler(db *sqlx.DB) *sqlxHandler { return &sqlxHandler{db} }
