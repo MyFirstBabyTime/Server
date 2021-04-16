820c5e08e3eb9cf1d382eaa26a28549695f530ee
지농 <jinhong0719@naver.com>
2021-04-16 20:09:02 +0900

    [ADD] Add parentPhoneCertifyRepository struct & contructor

--- auth/repository/mysql/repo_ParentPhoneCertify.go
+++ auth/repository/mysql/repo_ParentPhoneCertify.go
@@ -1,1 +1,28 @@
 package mysql
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"log"
+
+	"github.com/MyFirstBabyTime/Server/domain"
+)
+
+// parentPhoneCertifyRepository is implementation of domain.AuthRepository using mysql
+type parentPhoneCertifyRepository struct {
+	db           *sqlx.DB
+	migrator     migrator
+	sqlMsgParser sqlMsgParser
+}
+
+// ParentPhoneCertifyRepository return implementation of domain.ParentPhoneCertifyRepository using mysql
+func ParentPhoneCertifyRepository(db *sqlx.DB, sp sqlMsgParser) domain.ParentPhoneCertifyRepository {
+	repo := &parentPhoneCertifyRepository{
+		db:           db,
+		sqlMsgParser: sp,
+	}
+	if err := repo.migrator.MigrateModel(repo.db, domain.ParentPhoneCertify{}); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to migrate parent phone certify").Error())
+	}
+	return repo
+}
