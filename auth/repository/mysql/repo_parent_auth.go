2542da305cc104dce23d83146696301eee447bb4
지농 <jinhong0719@naver.com>
2021-04-16 11:50:46 +0900

    [MODIFY] <auth/repository/mysql/repo_parent_auth.go> Change how to migrate

--- auth/repository/mysql/repo_parent_auth.go
+++ auth/repository/mysql/repo_parent_auth.go
@@ -1,9 +1,6 @@
 package mysql
 
 import (
-	"github.com/Masterminds/squirrel"
-	"github.com/VividCortex/mysqlerr"
-	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"log"
@@ -13,49 +10,21 @@ import (
 )
 
 // mysqlAuthRepository is implementation of domain.AuthRepository using mysql
-type mysqlAuthRepository struct {
+type parentAuthRepository struct {
+	migrator migrator
 	db *sqlx.DB
 }
 
-// NewMysqlAuthRepository return implementation of domain.AuthRepository using mysql
-func NewMysqlAuthRepository(db *sqlx.DB) domain.ParentAuthRepository {
-	repo := &mysqlAuthRepository{db}
-
-	if err := repo.migrateModel(domain.ParentAuth{}); err != nil {
+// ParentAuthRepository return implementation of domain.ParentAuthRepository using mysql
+func ParentAuthRepository(db *sqlx.DB) domain.ParentAuthRepository {
+	repo := &parentAuthRepository{db: db}
+	if err := repo.migrator.MigrateModel(repo.db, domain.ParentAuth{}); err != nil {
 		log.Fatal(errors.Wrap(err, "failed to migrate parent auth model").Error())
-	}
-
-	if err := repo.migrateModel(domain.ParentPhoneCertify{}); err != nil {
-		log.Fatal(errors.Wrap(err, "failed to migrate parent phone number certify").Error())
 	}
 	return repo
 }
 
 // Store is implement domain.AuthRepository interface
-func (ar *mysqlAuthRepository) Store(ctx tx.Context, pa *domain.ParentAuth) (err error) {
-	return
-}
-
-// migrateModel method migrate model received from parameter to this repository
-func (ar *mysqlAuthRepository) migrateModel(model interface {
-	TableName() string // TableName return table name about model
-	Schema() string    // Schema return schema SQL about model
-}) (err error) {
-	_sql, _, _ := squirrel.Select("*").From(model.TableName()).ToSql()
-	switch _, err = ar.db.Query(_sql); tErr := err.(type) {
-	case nil:
-		break
-	case *mysql.MySQLError:
-		switch tErr.Number {
-		case mysqlerr.ER_NO_SUCH_TABLE:
-			_, err = ar.db.Exec(model.Schema())
-			err = errors.Wrapf(err, "failed to exec %s model schema", model.TableName())
-		default:
-			err = errors.Wrapf(err, "check table query returns unexpected mysql error code")
-		}
-	default:
-		err = errors.Wrapf(err, "check table query returns unexpected error type")
-	}
-
+func (ar *parentAuthRepository) Store(ctx tx.Context, pa *domain.ParentAuth) (err error) {
 	return
 }
