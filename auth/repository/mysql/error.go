267b3d6ceca2ecae64942acece178afbc3ff399b
지농 <jinhong0719@naver.com>
2021-04-16 16:25:47 +0900

    [ADD] <auth/repository/mysql/error.go> Add rowNotExistErr type

--- auth/repository/mysql/error.go
+++ auth/repository/mysql/error.go
@@ -1,1 +1,8 @@
 package mysql
+
+// rowNotExistErr is error type & used for row not exist error
+type rowNotExistErr struct {
+	error
+}
+func (err rowNotExistErr) Error() string { return err.error.Error() }
+func (err rowNotExistErr) RowNotExist() bool { return true }
