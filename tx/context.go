549ca13f281c94cce30954c03089fea34fcfff53
지농 <jinhong0719@naver.com>
2021-04-16 11:39:33 +0900

    [ADD] <tx/context.go> Add Context interface

--- tx/context.go
+++ tx/context.go
@@ -1,2 +1,10 @@
 package tx
 
+import "context"
+
+// Context is interface have func about get & set TX value with embedded context.Context
+type Context interface {
+	context.Context
+	Tx() (tx interface{})
+	SetTx(tx interface{})
+}
