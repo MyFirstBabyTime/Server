7fa4af3033209e2ff89a0b6e902b41afdb741581
지농 <jinhong0719@naver.com>
2021-04-16 19:59:41 +0900

    [ADD] <parser/mysql_parser.go> Add EntryDuplicate method

--- parser/mysql_parser.go
+++ parser/mysql_parser.go
@@ -1,6 +1,22 @@
 package parser
 
+import (
+	"fmt"
+	"strings"
+)
+
 // mysqlMsgParser is used for parse mysql msg 
 type mysqlMsgParser struct {}
 func MysqlMsgParser() *mysqlMsgParser { return new(mysqlMsgParser) }
+
+// EntryDuplicate method parse & return entry, key from mysql message
+func (mp *mysqlMsgParser) EntryDuplicate(msg string) (entry, key string) {
+	// Ex) Duplicate entry 'testID' for key 'id' -> Duplicate entry testID for key id
+	msg = strings.ReplaceAll(msg, "'", "")
+
+	if _, err := fmt.Sscanf(msg, "Duplicate entry %s for key %s", &entry, &key); err != nil {
+		entry, key = "", ""
+	}
+	return
+}
 
