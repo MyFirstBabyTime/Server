dda7b6688bfce3e20ad5e0371111108c9fc1db7b
지농 <jinhong0719@naver.com>
2021-04-18 14:55:49 +0900

    [ADD] <auth/repository/mysql/error.go> Add noReferencedRowErr

--- auth/repository/mysql/error.go
+++ auth/repository/mysql/error.go
@@ -34,3 +34,22 @@ func isEntryDuplicate(err error) (bool, entryDuplicate) {
 	ee, ok := err.(entryDuplicate)
 	return ok && ee.IsEntryDuplicate(), ee
 }
+
+// noReferencedRowErr is error type & used for no referenced row error
+type noReferencedRowErr struct {
+	error
+	foreignKey string
+}
+func (nre noReferencedRowErr) Error() string { return nre.error.Error() }
+func (nre noReferencedRowErr) IsNoReferenced() bool { return true }
+func (nre noReferencedRowErr) ForeignKey() string { return nre.foreignKey }
+
+// noReferencedRow interface & isNoReferencedRow method is used for check & get error context
+type noReferencedRow interface {
+	IsNoReferencedRow() bool
+	ForeignKey() string
+}
+func isNoReferencedRow(err error) (bool, noReferencedRow) {
+	nr, ok := err.(noReferencedRow)
+	return ok && nr.IsNoReferencedRow(), nr
+}
