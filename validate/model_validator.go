0baf46a0e05d0562ab7f7cdcbbb1e4583c94e176
지농 <jinhong0719@naver.com>
2021-04-18 16:10:05 +0900

    [ADD] <validate/model_validator.go> Add intializing validator

--- validate/model_validator.go
+++ validate/model_validator.go
@@ -1,5 +1,30 @@
 package validate
 
+import (
+	"database/sql"
+	"github.com/go-playground/validator/v10"
+)
+
+// modelValidatorInstance is global variable returned in ModelValidator function
+var modelValidatorInstance *modelValidator
+
+// initialize modelValidatorInstance with registering custom validation
+func init() {
+	v := validator.New()
+
+	_ = v.RegisterValidation("uuid", isValidateUUID)
+	_ = v.RegisterValidation("range", isWithinRange)
+
+	v.RegisterCustomTypeFunc(sqlNullStringType, sql.NullString{})
+
+	modelValidatorInstance = &modelValidator{v}
+}
+
+// ModelValidator function return modelValidatorInstance global variable
+func ModelValidator() *modelValidator {
+	return modelValidatorInstance
+}
+
 // modelValidator is struct embedding *validator.Validate which is registered custom validation
 type modelValidator struct {
 	*validator.Validate
