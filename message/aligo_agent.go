22dd85f0cfc66019baa8624ed13bf6f490d57bfb
지농 <jinhong0719@naver.com>
2021-04-18 21:48:06 +0900

    [ADD] <message/aligo_agent.go> Add aligoAgent struct

--- message/aligo_agent.go
+++ message/aligo_agent.go
@@ -1,1 +1,14 @@
 package message
+
+// aligoAgent is struct that agent API about aligo including sending message, get message list, etc ...
+type aligoAgent struct {
+	apiKey, id, sender string
+}
+
+func AligoAgent(apiKey, id, sender string) *aligoAgent {
+	return &aligoAgent{
+		apiKey: apiKey,
+		id:     id,
+		sender: sender,
+	}
+}
