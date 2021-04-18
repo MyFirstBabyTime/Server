cfbdeda4d5e3fa5bc616b45fcb57f2d0a78a4bac
지농 <jinhong0719@naver.com>
2021-04-18 17:35:40 +0900

    [MODIFY] <domain/auth.go> Change AuthUsecase interface

--- domain/auth.go
+++ domain/auth.go
@@ -7,23 +7,24 @@ import (
 
 // AuthUsecase is abstract interface about usecase layer using in delivery layer
 type AuthUsecase interface {
-	// SendPhoneNumberCertifyCode method send phone number certify code
-	SendPhoneNumberCertifyCode(ctx context.Context)
+	// SendCertifyCodeToPhone method send certify code to phone with pn(phone number)
+	SendCertifyCodeToPhone(ctx context.Context, pn string) error
 
-	// CertifyPhoneNumber method certify phone number with certify code
-	CertifyPhoneNumber(ctx context.Context)
+	// CertifyPhoneWithCode method certify phone with certify code
+	CertifyPhoneWithCode(ctx context.Context, pn string, code int) error
 
-	// SignUpParent method create new parent auth with parent phone number
-	SignUpParent(ctx context.Context)
+	// SignUpParent method create new parent auth with ParentAuthparent phone number
+	SignUpParent(ctx context.Context, pa *ParentAuth, pn string) error
+
+	// LoginParentAuth method login parent auth & return logged ParentAuth model, token
+	LoginParentAuth(ctx context.Context, id, pw string) (uuid, token string, err error)
 }
 
-// AuthRepository is abstract interface about repository layer using in usecase layer
-//type AuthRepositoryTXHandler interface {
-	//parentAuthRepository
-
-	//BeginTx(context.Context, *sql.TxOptions) (*sqlx.Tx, error) // BeginTx method start transaction
-	//Commit(tx *sqlx.Tx) (err error)                            // Commit method commit transaction
-	//Rollback(tx *sqlx.Tx) (err error)                          // Rollback method rollback transaction
+//TXHandler will be moved into usecase layer
+//type TXHandler interface {
+//	BeginTx(ctx context.Context, opts interface{}) (ctx context.TX, err error) // BeginTx method start transaction (get option from ctx)
+//	Commit(tx tx.Context) (err error)                       // Commit method commit transaction
+//	Rollback(tx tx.Context) (err error)                     // Rollback method rollback transaction
 //}
 
 // ParentAuthRepository is repository interface about ParentAuth model
