9f128dabd1d485023f43ad4dc6449ed9c6692b38
지농 <jinhong0719@naver.com>
2021-04-18 16:08:05 +0900

    [ADD] <validate/baked_in.go> Add validating function

--- validate/baked_in.go
+++ validate/baked_in.go
@@ -1,1 +1,40 @@
 package validate
+
+import (
+	"github.com/go-playground/validator/v10"
+	"strconv"
+	"strings"
+)
+
+// isValidateUUID function return if uuid format is validate
+func isValidateUUID(fl validator.FieldLevel) bool {
+	if fl.Field().String() == "" {
+		return true
+	}
+
+	switch fl.Param() {
+	case "parent":
+		return parentUUIDRegex.MatchString(fl.Field().String())
+	}
+	return false
+}
+
+// isWithinRange function return if field value is within range
+func isWithinRange(fl validator.FieldLevel) bool {
+	_range := strings.Split(fl.Param(), "~")
+	if len(_range) != 2 {
+		return false
+	}
+
+	start, err := strconv.Atoi(_range[0])
+	if err != nil {
+		return false
+	}
+	end, err := strconv.Atoi(_range[1])
+	if err != nil {
+		return false
+	}
+
+	field := int(fl.Field().Int())
+	return field >= start && field <= end
+}
