8158fb640c3a08fffa4ddcec19002397e060215b
지농 <jinhong0719@naver.com>
2021-04-18 21:01:23 +0900

    [ADD] <auth/usecase/error.go> Add conflictErr struct

--- auth/usecase/error.go
+++ auth/usecase/error.go
@@ -1,5 +1,13 @@
 package usecase
 
+// conflictErr is error type & used for conflict status
+type conflictErr struct {
+	error
+	code int
+}
+func (_ conflictErr) Conflict() {}
+func (err conflictErr) Code() int { return err.code }
+
 // internalServerErr is error type & used for internal server status
 type internalServerErr struct {
 	error
