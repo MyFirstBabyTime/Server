7ec6859aaf422cf37a3b6fecb87942faf9f16f4a
지농 <jinhong0719@naver.com>
2021-04-18 21:03:32 +0900

    [ADD] <auth/usecase/auth_ucase.go> Add field in authUsecase

--- auth/usecase/auth_ucase.go
+++ auth/usecase/auth_ucase.go
@@ -1,10 +1,21 @@
 package usecase
 
-import "github.com/MyFirstBabyTime/Server/domain"
+import (
+
+	"github.com/MyFirstBabyTime/Server/domain"
+)
 
 // authUsecase is used for usecase layer which implement domain.AuthUsecase interface
 type authUsecase struct {
 
+	// parentAuthRepository is repository interface about domain.ParentAuth model
+	parentAuthRepository domain.ParentAuthRepository
+
+	// parentPhoneCertifyRepository is repository interface about domain.ParentPhoneCertify model
+	parentPhoneCertifyRepository domain.ParentPhoneCertifyRepository
+
+	// txHandler is used for handling transaction to begin & commit or rollback
+	txHandler TxHandler
 }
 
 // AuthUsecase return implementation of domain.AuthUsecase
