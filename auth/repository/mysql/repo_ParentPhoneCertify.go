d1124297f23ea5ee085cd0652c045b4b92ffb196
지농 <jinhong0719@naver.com>
2021-04-18 14:57:50 +0900

    [ADD] <auth/repository/mysql/repo_ParentAuth.go> Add Store

--- auth/repository/mysql/repo_ParentPhoneCertify.go
+++ auth/repository/mysql/repo_ParentPhoneCertify.go
@@ -3,12 +3,14 @@ package mysql
 import (
 	"database/sql"
 	"github.com/Masterminds/squirrel"
-	"github.com/MyFirstBabyTime/Server/tx"
+	"github.com/VividCortex/mysqlerr"
+	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"log"
 
 	"github.com/MyFirstBabyTime/Server/domain"
+	"github.com/MyFirstBabyTime/Server/tx"
 )
 
 // parentPhoneCertifyRepository is implementation of domain.AuthRepository using mysql
@@ -32,17 +34,49 @@ func ParentPhoneCertifyRepository(db *sqlx.DB, sp sqlMsgParser) domain.ParentPho
 	return repo
 }
 
-func (pp *parentPhoneCertifyRepository) GetByPhoneNumber(ctx tx.Context, pn string) (certify domain.ParentPhoneCertify, err error) {
+func (pp *parentPhoneCertifyRepository) GetByPhoneNumber(ctx tx.Context, pn string) (ppc domain.ParentPhoneCertify, err error) {
 	_tx, _ := ctx.Tx().(*sqlx.Tx)
 	_sql, args, _ := squirrel.Select("*").From("parent_phone_certify").Where("phone_number = ?", pn).ToSql()
 
-	switch err = _tx.Get(&certify, _sql, args...); err {
+	switch err = _tx.Get(&ppc, _sql, args...); err {
 	case nil:
 		break
 	case sql.ErrNoRows:
 		err = rowNotExistErr{errors.Wrap(err, "failed to select parent phone certify")}
 	default:
 		err = errors.Wrap(err, "select parent phone certify return unexpected error")
+	}
+	return
+}
+
+func (pp *parentPhoneCertifyRepository) Store(ctx tx.Context, ppc *domain.ParentPhoneCertify) (err error) {
+	if ppc.CertifyCode == 0 {
+		ppc.CertifyCode = ppc.GenerateCertifyCode()
+	}
+
+	_tx, _ := ctx.Tx().(*sqlx.Tx)
+	_sql, args, _ := squirrel.Insert("parent_phone_certify").
+		Columns("parent_uuid", "phone_number", "certify_code").
+		Values(ppc.ParentUUID, ppc.PhoneNumber, ppc.CertifyCode).ToSql()
+
+	switch _, err = _tx.Exec(_sql, args...); tErr := err.(type) {
+	case nil:
+		break
+	case *mysql.MySQLError:
+		switch tErr.Number {
+		case mysqlerr.ER_DUP_ENTRY:
+			err = errors.Wrap(err, "failed to insert parent phone certify")
+			_, key := pp.sqlMsgParser.EntryDuplicate(tErr.Message)
+			err = entryDuplicateErr{err, key}
+		case mysqlerr.ER_NO_REFERENCED_ROW_2:
+			err = errors.Wrap(err, "failed to insert parent phone certify")
+			fk := pp.sqlMsgParser.NoReferencedRow(tErr.Message)
+			err = noReferencedRowErr{err, fk}
+		default:
+			err = errors.Wrap(err, "insert parent auth return unexpected code return")
+		}
+	default:
+		err = errors.Wrap(err, "insert parent auth return unexpected error type")
 	}
 	return
 }
