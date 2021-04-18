9729ac730e75c7fc8f072df48f382432282bb32d
지농 <jinhong0719@naver.com>
2021-04-18 16:09:38 +0900

    [ADD] <validate/modql_validator.go> Add modelValidator type & method

--- validate/model_validator.go
+++ validate/model_validator.go
@@ -1,1 +1,10 @@
 package validate
+
+// modelValidator is struct embedding *validator.Validate which is registered custom validation
+type modelValidator struct {
+	*validator.Validate
+}
+
+func (mv *modelValidator) ValidateStruct(s interface{}) error {
+	return mv.Struct(s)
+}
