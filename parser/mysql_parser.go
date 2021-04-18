aee196c95b757c98119bc7eb7e3ff7c9c0bbbd15
지농 <jinhong0719@naver.com>
2021-04-18 16:07:07 +0900

    [ADD] <parser/mysql_parser.go> Add NoReferencedRow method

--- parser/mysql_parser.go
+++ parser/mysql_parser.go
@@ -9,7 +9,7 @@ import (
 type mysqlMsgParser struct {}
 func MysqlMsgParser() *mysqlMsgParser { return new(mysqlMsgParser) }
 
-// EntryDuplicate method parse & return entry, key from mysql message
+// EntryDuplicate method parse & return entry, key from mysql entry duplicate error message
 func (mp *mysqlMsgParser) EntryDuplicate(msg string) (entry, key string) {
 	// Ex) Duplicate entry 'testID' for key 'id' -> Duplicate entry testID for key id
 	msg = strings.ReplaceAll(msg, "'", "")
@@ -20,3 +20,18 @@ func (mp *mysqlMsgParser) EntryDuplicate(msg string) (entry, key string) {
 	return
 }
 
+// NoReferencedRow method parse & return foreign key from mysql no referenced error message
+func (mp *mysqlMsgParser) NoReferencedRow(msg string) string {
+	var msgFmt = "Cannot add or update a child row: a foreign key constraint fails %s CONSTRAINT %s FOREIGN KEY %s REFERENCES %s"
+	for _, i := range []rune{'`', ',', '(', ')'} {
+		msg = strings.ReplaceAll(msg, string(i), "")
+	}
+
+	bind := struct {
+		fkTable, constraint, fk, refTable string
+	}{}
+	if _, err := fmt.Sscanf(msg, msgFmt, &bind.fkTable, &bind.constraint, &bind.fk, &bind.refTable); err != nil {
+		bind.fk = ""
+	}
+	return bind.fk
+}
