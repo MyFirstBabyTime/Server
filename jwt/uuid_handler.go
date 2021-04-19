785937d1e5320ef3865ffdd091417ed08c228e8a
지농 <jinhong0719@naver.com>
2021-04-19 11:30:50 +0900

    [ADD] <jwt/uuid_handler.go> Add uuidHandler struct

--- jwt/uuid_handler.go
+++ jwt/uuid_handler.go
@@ -1,1 +1,12 @@
 package jwt
+
+// uuidHandler is jwt handler about uuid token
+type uuidHandler struct {
+	jwtKey string
+}
+
+func UUIDHandler(key string) *uuidHandler {
+	return &uuidHandler{
+		jwtKey: key,
+	}
+}
