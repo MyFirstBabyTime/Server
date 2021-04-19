a250719e3ad161b39290042315f4150d4c2e5a8b
지농 <jinhong0719@naver.com>
2021-04-19 09:24:39 +0900

    [ADD] <hash/bcrypt_handler.go> Add bcryptHandler method

--- hash/bcrypt_handler.go
+++ hash/bcrypt_handler.go
@@ -1,8 +1,25 @@
 package hash
 
+import "golang.org/x/crypto/bcrypt"
+
 // bcryptHandler is hash handler using bcrypt algorithm
 type bcryptHandler struct {}
 
 func BcryptHandler() *bcryptHandler {
 	return &bcryptHandler{}
+}
+
+// GenerateHashWithMinSalt generate & return hashed value from password with minimum salt
+func (bh *bcryptHandler) GenerateHashWithMinSalt(pw string) (string, error) {
+	return bh.generateHashFromPW(pw, bcrypt.MinCost)
+}
+
+// CompareHashAndPW compare hashed value and password & return error
+func (bh *bcryptHandler) CompareHashAndPW(hash, pw string) (err error) {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw))
+}
+
+func (bh *bcryptHandler) generateHashFromPW(pw string, salt int) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(pw), salt)
+	return string(b), err
 }
