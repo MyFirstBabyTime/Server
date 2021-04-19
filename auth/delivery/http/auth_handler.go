bf5ef61809f38323a61cea9c78cdae17aba59f76
지농 <jinhong0719@naver.com>
2021-04-19 15:43:28 +0900

    [ADD] <auth/delivery/http/request.go> Add LoginParentAuth method

--- auth/delivery/http/auth_handler.go
+++ auth/delivery/http/auth_handler.go
@@ -28,9 +28,10 @@ func NewAuthHandler(r *gin.Engine, au domain.AuthUsecase, v validator) {
 	r.POST("phones/phone-number/:phone_number/certify-code", h.SendCertifyCodeToPhone)
 	r.POST("phones/phone-number/:phone_number/certification", h.CertifyPhoneWithCode)
 	r.POST("parents", h.SignUpParent)
+	r.POST("login/parent", h.LoginParentAuth)
 }
 
-// SendCertifyCodeToPhone is implement domain.AuthUsecase interface
+// SendCertifyCodeToPhone deliver data to SendCertifyCodeToPhone of domain.AuthUsecase
 func (ah *authHandler) SendCertifyCodeToPhone(c *gin.Context) {
 	req := new(sendCertifyCodeToPhoneRequest)
 	if err := ah.bindRequest(req, c); err != nil {
@@ -53,7 +54,7 @@ func (ah *authHandler) SendCertifyCodeToPhone(c *gin.Context) {
 	return
 }
 
-// CertifyPhoneWithCode is implement domain.AuthUsecase interface
+// CertifyPhoneWithCode deliver data to CertifyPhoneWithCode of domain.AuthUsecase
 func (ah *authHandler) CertifyPhoneWithCode(c *gin.Context) {
 	req := new(certifyPhoneWithCodeRequest)
 	if err := ah.bindRequest(req, c); err != nil {
@@ -74,7 +75,7 @@ func (ah *authHandler) CertifyPhoneWithCode(c *gin.Context) {
 	return
 }
 
-// SignUpParent is implement domain.AuthUsecase interface
+// SignUpParent deliver data to SignUpParent of domain.AuthUsecase
 func (ah *authHandler) SignUpParent(c *gin.Context) {
 	req := new(signUpParentRequest)
 	if err := ah.bindRequest(req, c); err != nil {
@@ -86,11 +87,34 @@ func (ah *authHandler) SignUpParent(c *gin.Context) {
 	switch err := ah.aUsecase.SignUpParent(c.Request.Context(), pa, req.PhoneNumber); tErr := err.(type) {
 	case nil:
 		resp := defaultResp(http.StatusCreated, 0, "succeed to sign up new parent auth")
-		c.JSON(http.StatusOK, resp)
+		c.JSON(http.StatusCreated, resp)
 	case usecaseErr:
 		c.JSON(tErr.Status(), defaultResp(tErr.Status(), tErr.Code(), tErr.Error()))
 	default:
 		msg := errors.Wrap(err, "SignUpParent return unexpected error").Error()
+		c.JSON(http.StatusInternalServerError, defaultResp(http.StatusInternalServerError, 0, msg))
+	}
+	return
+}
+
+// LoginParentAuth deliver data to LoginParentAuth of domain.AuthUsecase
+func (ah *authHandler) LoginParentAuth(c *gin.Context) {
+	req := new(loginParentAuthRequest)
+	if err := ah.bindRequest(req, c); err != nil {
+		c.JSON(http.StatusBadRequest, defaultResp(http.StatusBadRequest, 0, err.Error()))
+		return
+	}
+
+	uuid, token, err := ah.aUsecase.LoginParentAuth(c.Request.Context(), req.ID, req.PW)
+	switch tErr := err.(type) {
+	case nil:
+		resp := defaultResp(http.StatusOK, 0, "succeed to login parent auth")
+		resp["uuid"], resp["token"] = uuid, token
+		c.JSON(http.StatusOK, resp)
+	case usecaseErr:
+		c.JSON(tErr.Status(), defaultResp(tErr.Status(), tErr.Code(), tErr.Error()))
+	default:
+		msg := errors.Wrap(err, "LoginParentAuth return unexpected error").Error()
 		c.JSON(http.StatusInternalServerError, defaultResp(http.StatusInternalServerError, 0, msg))
 	}
 	return
