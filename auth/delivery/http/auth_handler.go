78d26442b61a6b8875ca4ae9fcfd5e09fb6ee9df
지농 <jinhong0719@naver.com>
2021-04-19 14:12:18 +0900

    [ADD] <auth/delivery/http/auth_handler.go> Add SendCertifyCodeToPhone

--- auth/delivery/http/auth_handler.go
+++ auth/delivery/http/auth_handler.go
@@ -1,5 +1,12 @@
 package http
 
+import (
+	"github.com/MyFirstBabyTime/Server/domain"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
 // authHandler represent the http handler for article
 type authHandler struct {
 	aUsecase  domain.AuthUsecase
@@ -18,7 +25,41 @@ func NewAuthHandler(e *gin.Engine, au domain.AuthUsecase, v validator) {
 		validator: v,
 	}
 
+	e.POST("phones/phone-number/:phone_number/certify-code", h.SendCertifyCodeToPhone)
+	//e.GET("/articles/:id", handler.GetByID)
+	//e.DELETE("/articles/:id", handler.Delete)
 }
+
+// SendCertifyCodeToPhone is implement domain.AuthUsecase interface
+func (ah *authHandler) SendCertifyCodeToPhone(c *gin.Context) {
+	req := new(sendCertifyCodeToPhoneRequest)
+	if err := c.BindUri(req); err != nil {
+		resp := defaultResp(http.StatusUnprocessableEntity, 0, errors.Wrap(err, "failed to bind req").Error())
+		c.JSON(http.StatusUnprocessableEntity, resp)
+		return
+	}
+
+	if err := ah.validator.ValidateStruct(req); err != nil {
+		resp := defaultResp(http.StatusBadRequest, 0, errors.Wrap(err, "invalid request").Error())
+		c.JSON(http.StatusBadRequest, resp)
+		return
+	}
+
+	switch err := ah.aUsecase.SendCertifyCodeToPhone(c.Request.Context(), req.PhoneNumber); tErr := err.(type) {
+	case nil:
+		resp := defaultResp(http.StatusOK, 0, "succeed to send certify code to phone")
+		c.JSON(http.StatusOK, resp)
+	case usecaseErr:
+		resp := defaultResp(tErr.Status(), tErr.Code(), tErr.Error())
+		c.JSON(tErr.Status(), resp)
+	default:
+		msg := errors.Wrap(err, "SendCertifyCodeToPhone return unexpected error").Error()
+		resp := defaultResp(http.StatusInternalServerError, 0, msg)
+		c.JSON(http.StatusInternalServerError, resp)
+	}
+	return
+}
+
 func defaultResp(status, code int, msg string) interface{} {
 	return struct {
 		Status  int    `json:"status"`
