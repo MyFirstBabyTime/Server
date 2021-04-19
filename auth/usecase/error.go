2bcfee2f404fec685a8d124666e721e01b4f4f86
지농 <jinhong0719@naver.com>
2021-04-19 13:54:28 +0900

    [MODIFY] <auth/usecase/error.go> Change all error type

--- auth/usecase/error.go
+++ auth/usecase/error.go
@@ -2,25 +2,40 @@ package usecase
 
 import "net/http"
 
-// notFoundErr is error type & used for not found status
-type notFoundErr struct {
+type baseErr struct {
 	error
 }
-func (_ notFoundErr) NotFound() {}
-func (err notFoundErr) Status() int { return http.StatusNotFound }
+func (_ baseErr) Status() (_ int) { return }
+func (_ baseErr) Code() (_ int) { return }
+
+// notFoundErr is error type & used for not found status
+type _notFoundErr struct {
+	baseErr
+}
+func notFoundErr(err error) _notFoundErr {
+	return _notFoundErr{baseErr{err}}
+}
+func (_ _notFoundErr) NotFound() {}
+func (err _notFoundErr) Status() int { return http.StatusNotFound }
 
 // conflictErr is error type & used for conflict status
-type conflictErr struct {
-	error
+type _conflictErr struct {
+	baseErr
 	code int
 }
-func (_ conflictErr) Conflict() {}
-func (err conflictErr) Status() int { return http.StatusConflict }
-func (err conflictErr) Code() int { return err.code }
+func conflictErr(err error, code int) _conflictErr {
+	return _conflictErr{baseErr{err}, code}
+}
+func (_ _conflictErr) Conflict() {}
+func (err _conflictErr) Status() int { return http.StatusConflict }
+func (err _conflictErr) Code() int { return err.code }
 
 // internalServerErr is error type & used for internal server status
-type internalServerErr struct {
-	error
+type _internalServerErr struct {
+	baseErr
 }
-func (_ internalServerErr) InternalServer() {}
-func (err internalServerErr) Status() int { return http.StatusInternalServerError }
+func internalServerErr(err error) _internalServerErr {
+	return _internalServerErr{baseErr{err}}
+}
+func (_ _internalServerErr) InternalServer() {}
+func (err _internalServerErr) Status() int { return http.StatusInternalServerError }
