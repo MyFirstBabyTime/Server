a2ac27bd5fa37e0160db6a16acdef7f55a3fa79e
지농 <jinhong0719@naver.com>
2021-04-19 14:11:15 +0900

    [ADD] <auth/delivery/http/auth_handler.go> Add authHandler

--- auth/delivery/http/auth_handler.go
+++ auth/delivery/http/auth_handler.go
@@ -1,1 +1,16 @@
 package http
+
+// authHandler represent the http handler for article
+type authHandler struct {
+	aUsecase  domain.AuthUsecase
+	validator validator
+}
+
+// NewAuthHandler will initialize the auth/ resources endpoint
+func NewAuthHandler(e *gin.Engine, au domain.AuthUsecase, v validator) {
+	h := &authHandler{
+		aUsecase:  au,
+		validator: v,
+	}
+
+}
