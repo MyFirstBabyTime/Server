14f28b89d0e9192f413a0021575e48d47c4d2898
지농 <jinhong0719@naver.com>
2021-04-19 14:36:43 +0900

    [ADD] <auth/delivery/http/request.go> Add certifyPhoneWithCodeRequest

--- auth/delivery/http/request.go
+++ auth/delivery/http/request.go
@@ -13,3 +13,20 @@ type sendCertifyCodeToPhoneRequest struct {
 func (r *sendCertifyCodeToPhoneRequest) BindFrom(c *gin.Context) error {
 	return errors.Wrap(c.BindUri(r), "failed to BindUri")
 }
+
+// certifyPhoneWithCodeRequest is request for authHandler.CertifyPhoneWithCode
+type certifyPhoneWithCodeRequest struct {
+	PhoneNumber string `uri:"phone_number" validate:"required"`
+	CertifyCode int64  `json:"certify_code" validate:"required"`
+}
+
+func (r *certifyPhoneWithCodeRequest) BindFrom(c *gin.Context) error {
+	if err := c.BindUri(r); err != nil {
+		return errors.Wrap(err, "failed to BindUri")
+	}
+
+	if err := c.BindJSON(r); err != nil {
+		return errors.Wrap(err, "failed to BindJSON")
+	}
+	return nil
+}
