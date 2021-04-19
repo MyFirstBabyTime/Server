863d34e9cd93294760805d0caf32888cb7723787
지농 <jinhong0719@naver.com>
2021-04-19 14:12:33 +0900

    [ADD] <auth/delivery/http/request.go> Add sendCertifyCodeToPhoneRequest

--- auth/delivery/http/request.go
+++ auth/delivery/http/request.go
@@ -1,1 +1,6 @@
 package http
+
+// sendCertifyCodeToPhoneRequest is request for authHandler.SendCertifyCodeToPhone
+type sendCertifyCodeToPhoneRequest struct {
+	PhoneNumber string `uri:"phone_number" validate:"required,len=11"`
+}
