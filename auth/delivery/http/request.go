3f3b2e78a8e996f3c609c1c2d8f3f7fb64a1690a
지농 <jinhong0719@naver.com>
2021-04-19 14:36:24 +0900

    [ADD] <auth/delivery/http/request.go> Add BindFrom method

--- auth/delivery/http/request.go
+++ auth/delivery/http/request.go
@@ -1,6 +1,15 @@
 package http
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+)
+
 // sendCertifyCodeToPhoneRequest is request for authHandler.SendCertifyCodeToPhone
 type sendCertifyCodeToPhoneRequest struct {
 	PhoneNumber string `uri:"phone_number" validate:"required,len=11"`
+}
+
+func (r *sendCertifyCodeToPhoneRequest) BindFrom(c *gin.Context) error {
+	return errors.Wrap(c.BindUri(r), "failed to BindUri")
 }
