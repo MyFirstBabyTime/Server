e1486d6ad89ff347c687d2a78df8e31e2af0d1e6
지농 <jinhong0719@naver.com>
2021-04-19 14:11:31 +0900

    [ADD] <auth/delivery/http/auth_handler.go> Add validator

--- auth/delivery/http/auth_handler.go
+++ auth/delivery/http/auth_handler.go
@@ -6,6 +6,11 @@ type authHandler struct {
 	validator validator
 }
 
+// validator is interface used for validating struct value
+type validator interface {
+	ValidateStruct(s interface{}) (err error)
+}
+
 // NewAuthHandler will initialize the auth/ resources endpoint
 func NewAuthHandler(e *gin.Engine, au domain.AuthUsecase, v validator) {
 	h := &authHandler{
