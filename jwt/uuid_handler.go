55cae759772d14970553c8753b4d088d8ead2415
지농 <jinhong0719@naver.com>
2021-04-19 11:31:04 +0900

    [ADD] <jwt/uuid_handler.go> Add GenerateUUIDJWT method

--- jwt/uuid_handler.go
+++ jwt/uuid_handler.go
@@ -1,5 +1,10 @@
 package jwt
 
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
 // uuidHandler is jwt handler about uuid token
 type uuidHandler struct {
 	jwtKey string
@@ -9,4 +14,23 @@ func UUIDHandler(key string) *uuidHandler {
 	return &uuidHandler{
 		jwtKey: key,
 	}
+}
+
+// uuidClaims is used for generate JWT including uuid inform
+type uuidClaims struct {
+	UUID string `json:"uuid"`
+	Type string `json:"type"`
+	jwt.StandardClaims
+}
+
+// GenerateUUIDJWT generate & return JWT UUID token with type & time
+func (uh *uuidHandler) GenerateUUIDJWT(uuid, _type string, t time.Duration) (token string, err error) {
+	token, err = jwt.NewWithClaims(jwt.SigningMethodHS512, uuidClaims{
+		UUID: uuid,
+		Type: _type,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(t).Unix(),
+		},
+	}).SignedString([]byte(uh.jwtKey))
+	return
 }
