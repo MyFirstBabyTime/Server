12c87d7d9dd653b5089cc12d73319442f30ec81b
지농 <jinhong0719@naver.com>
2021-04-20 14:32:01 +0900

    [MODIFY] <app/main.go> Change main.go

--- app/main.go
+++ app/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"github.com/MyFirstBabyTime/Server/app/config"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
+	"github.com/spf13/viper"
 	"log"
 
 	"github.com/MyFirstBabyTime/Server/hash"
@@ -15,6 +16,7 @@ import (
 	"github.com/MyFirstBabyTime/Server/tx"
 	"github.com/MyFirstBabyTime/Server/validate"
 
+	_authConfig "github.com/MyFirstBabyTime/Server/auth/config"
 	_authHttpDelivery "github.com/MyFirstBabyTime/Server/auth/delivery/http"
 	_authRepo "github.com/MyFirstBabyTime/Server/auth/repository/mysql"
 	_authUcase "github.com/MyFirstBabyTime/Server/auth/usecase"
@@ -23,11 +25,17 @@ import (
 func init() {
 	// set flag to log current date, time & long file name
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// set and read config file in viper package
+	viper.AutomaticEnv()
+	viper.SetConfigFile(config.App.ConfigFile())
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
 }
 
 func main() {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", "", "", "", "", "")
-	db, err := sqlx.Connect("mysql", dsn)
+	db, err := sqlx.Connect("mysql", config.App.MysqlDataSource())
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "failed to create mysql connection").Error())
 	}
@@ -37,13 +45,14 @@ func main() {
 	_ps := parser.MysqlMsgParser()
 	_vl := validate.New()
 	_tx := tx.NewSqlxHandler(db)
-	_msg := message.AligoAgent("", "", "")
+	_msg := message.AligoAgent(config.App.AligoAPIKey(), config.App.AligoAccountID(), config.App.AligoSender())
 	_hash := hash.BcryptHandler()
-	_jwt := jwt.UUIDHandler("")
+	_jwt := jwt.UUIDHandler(config.App.JwtKey())
 
 	au := _authUcase.AuthUsecase(
-		_authRepo.ParentAuthRepository(db, _ps, _vl),
-		_authRepo.ParentPhoneCertifyRepository(db, _ps, _vl),
+		_authConfig.App,
+		_authRepo.ParentAuthRepository(_authConfig.App, db, _ps, _vl),
+		_authRepo.ParentPhoneCertifyRepository(_authConfig.App, db, _ps, _vl),
 		_tx, _msg, _hash, _jwt,
 	)
 	_authHttpDelivery.NewAuthHandler(r, au, _vl)
