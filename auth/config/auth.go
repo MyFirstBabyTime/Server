33f6fbb016a8fa7f4463a7219dab37e3587204b4
지농 <jinhong0719@naver.com>
2021-04-20 14:31:17 +0900

    [ADD] <auth/config/auth.go> Add auth domain config

--- auth/config/auth.go
+++ auth/config/auth.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+	"time"
+)
+
+// App is the application config about syscheck domain
+var App *authConfig
+
+// init function initialize App global variable
+func init() {
+	App = &authConfig{}
+}
+
+// authConfig have config value and implement various interface about auth config
+type authConfig struct {
+	accessTokenDuration *time.Duration
+}
+
+// default const value about authConfig field
+const (
+	defaultAccessTokenDuration = time.Hour * 24
+)
+
+// AccessTokenDuration return access token valid duration
+func (ac *authConfig) AccessTokenDuration() time.Duration {
+	var key = "auth.accessTokenDuration"
+	if ac.accessTokenDuration != nil {
+		return *ac.accessTokenDuration
+	}
+
+	d, err := time.ParseDuration(viper.GetString(key))
+	if err != nil {
+		viper.Set(key, defaultAccessTokenDuration.String())
+		d = defaultAccessTokenDuration
+	}
+
+	ac.accessTokenDuration = &d
+	return *ac.accessTokenDuration
+}
