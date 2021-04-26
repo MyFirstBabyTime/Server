1a953b9afd23ad1b821267492f70d44bb94c2d1a
mspring03 <mspringmspring@naver.com>
2021-04-27 08:58:26 +0900

    [ADD] </usecase/cloud_maintainer_ucase.go> ContainerRedeploy

--- cloud-maintainer/usecase/cloud_maintainer_ucase.go
+++ cloud-maintainer/usecase/cloud_maintainer_ucase.go
@@ -22,3 +22,21 @@ func CloudMaintainerUsecase(cloudManagementKey string) domain.CloudMaintainerUse
 		cloudManagementKey: cloudManagementKey,
 	}
 }
+
+// ContainerRedeploy is implement domain.CloudMaintainerUsecase interface
+func (cu *cloudMaintainerUsecase) ContainerRedeploy(ctx context.Context, key string, image string) (err error) {
+	if cu.cloudManagementKey != key {
+		err = errors.New("cloudManagementKey is do not match")
+		err = domain.UsecaseError{UsecaseErr: err, Status: http.StatusForbidden}
+		return
+	}
+
+	go func() {
+		time.Sleep(time.Second)
+		b, err := exec.Command("docker", "service", "update", "--image", image, "FirstBabyTime_server").Output()
+		fmt.Println(string(b))
+		fmt.Println(err)
+	}()
+
+	return nil
+}
