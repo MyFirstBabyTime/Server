9131b17f572294da15da9b69350d4c935609cbbe
mspring03 <mspringmspring@naver.com>
2021-04-28 15:41:41 +0900

    [ADD] <validate/regexes.go> itemUUIDRegx

--- validate/regexes.go
+++ validate/regexes.go
@@ -4,8 +4,10 @@ import "regexp"
 
 const (
 	parentUUIDRegexString = "^p\\d{10}$"
+	itemUUIDRegexString = "^e\\d{10}$"
 )
 
 var (
 	parentUUIDRegex = regexp.MustCompile(parentUUIDRegexString)
+	itemUUIDRegx = regexp.MustCompile(itemUUIDRegexString)
 )
