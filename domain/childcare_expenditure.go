242ef039fbaffae474cd4776311fc079cc3489ef
mspring03 <mspringmspring@naver.com>
2021-04-29 15:06:02 +0900

    [ADD] <domain/childcare_expenditure.go> add Expenditure struct

--- domain/childcare_expenditure.go
+++ domain/childcare_expenditure.go
@@ -12,3 +12,14 @@ type ExpenditureUsecase interface {
 
 type ExpenditureRepository interface {
 }
+
+// Expenditure is model represent expenditure using in child_expenditure domain
+type Expenditure struct {
+	UUID       string         `db:"uuid" validate:"uuid=item"`
+	ParentUUID string         `db:"parent_uuid" validate:"required uuid=parent"`
+	BabyUUID   string         `db:"baby_uuid" validate:"required"`
+	Name       string         `db:"name" validate:"required"`
+	Amount     int64          `db:"amount" validate:"required"`
+	Rating     int64          `db:"rating" validate:"range=0~5"`
+	Link       sql.NullString `db:"link"`
+}
