b825488b7f827a27a06343f16c71ca1bcf189761
mspring03 <mspringmspring@naver.com>
2021-04-29 15:03:42 +0900

    [ADD] <repository/mysql/migrator.go> add migrateModel

--- chlidcare-expenditure/repository/mysql/migrator.go
+++ chlidcare-expenditure/repository/mysql/migrator.go
@@ -1,1 +1,34 @@
 package mysql
+
+import (
+	"github.com/Masterminds/squirrel"
+	"github.com/VividCortex/mysqlerr"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+type migrator struct{}
+
+func (m migrator) MigrateModel(db *sqlx.DB, model interface {
+	TableName() string
+	Schema() string
+}) (err error) {
+	sql, _, _ := squirrel.Select("*").From(model.TableName()).ToSql()
+	switch  _, err = db.Query(sql); tErr := err.(type) {
+	case nil:
+		break
+	case *mysql.MySQLError:
+		switch  tErr.Number {
+		case mysqlerr.ER_NO_SUCH_TABLE:
+			_, err = db.Exec(model.Schema())
+			err = errors.Wrapf(err, "failed to exec %s model schema", model.TableName())
+		default:
+			err = errors.Wrapf(err, "check table query returns unexpected mysql error code")
+		}
+	default:
+		err = errors.Wrapf(err, "check table query returns unexpected error type")
+	}
+
+	return
+}
