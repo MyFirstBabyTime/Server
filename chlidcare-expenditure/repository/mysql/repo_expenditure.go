fe5c20174ed81c5dc7ab60ec61f2246633702858
mspring03 <mspringmspring@naver.com>
2021-04-30 09:28:15 +0900

    [ADD] <repository/mysql/repo_expenditure.go> add ExpenditureRepo func

--- chlidcare-expenditure/repository/mysql/repo_expenditure.go
+++ chlidcare-expenditure/repository/mysql/repo_expenditure.go
@@ -1,5 +1,12 @@
 package mysql
 
+import (
+	"github.com/MyFirstBabyTime/Server/domain"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"log"
+)
+
 type expenditureRepository struct {
 	db        *sqlx.DB
 	migrator  migrator
@@ -9,4 +16,19 @@ type expenditureRepository struct {
 // validator is interface used for validating struct value
 type validator interface {
 	ValidateStruct(s interface{}) (err error)
+}
+
+func ExpenditureRepository(
+	db *sqlx.DB,
+	v validator,
+) domain.ExpenditureRepository {
+	repo := &expenditureRepository{
+		db:        db,
+		validator: v,
+	}
+
+	if err := repo.migrator.MigrateModel(repo.db, domain.Expenditure{}); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to migrate parent auth model").Error())
+	}
+	return repo
 }
