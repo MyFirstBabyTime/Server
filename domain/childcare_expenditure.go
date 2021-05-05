2209e7fc1241e3688f87fec471e34d76eddc939c
mspring03 <mspringmspring@naver.com>
2021-05-05 22:33:14 +0900

    [MODIFY] <domain/childcare_expenditure.go> Expenditure struct

--- domain/childcare_expenditure.go
+++ domain/childcare_expenditure.go
@@ -17,13 +17,12 @@ type ExpenditureRepository interface {
 
 // Expenditure is model represent expenditure using in child_expenditure domain
 type Expenditure struct {
-	UUID       string         `db:"uuid" validate:"uuid=item"`
-	ParentUUID string         `db:"parent_uuid" validate:"required uuid=parent"`
-	BabyUUID   string         `db:"baby_uuid" validate:"required"`
-	Name       string         `db:"name" validate:"required"`
-	Amount     int64          `db:"amount" validate:"required"`
-	Rating     int64          `db:"rating" validate:"range=0~5"`
-	Link       sql.NullString `db:"link"`
+	UUID       *string `db:"uuid" validate:"uuid=item"`
+	ParentUUID *string `db:"parent_uuid" validate:"required uuid=parent"`
+	Name       *string `db:"name" validate:"required"`
+	Amount     *int64  `db:"amount" validate:"required"`
+	Rating     *int64  `db:"rating" validate:"range=0~5"`
+	Link       *string `db:"link"`
 }
 
 // TableName return table name about Expenditure model
