862696c70f6ed4254cb625bb8cb91d8281b17d6b
mspring03 <mspringmspring@naver.com>
2021-05-05 22:05:50 +0900

    [ADD] <delivery/http/request.go> expenditure registration request struct

--- chlidcare-expenditure/delivery/http/request.go
+++ chlidcare-expenditure/delivery/http/request.go
@@ -4,3 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 )
+
+type expenditureRegistration struct {
+	ParentUUID string   `json:"parent_uuid" validate:"required,uuid=parent"`
+	BabyUUIDs  []string `json:"baby_uuids" validate:"required"`
+	Name       string   `json:"name" validate:"required"`
+	Amount     int64    `json:"amount" validate:"required"`
+	Rating     int64    `json:"rating" validate:"required,range=0~5"`
+	Link       string   `json:"link"`
+}
