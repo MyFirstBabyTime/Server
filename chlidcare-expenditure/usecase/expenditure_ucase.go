ecee63205690b30480d69387f8404e248fa8eac3
mspring03 <mspringmspring@naver.com>
2021-05-05 22:27:36 +0900

    [ADD] <usecase/expenditure_ucase.go> txHandler interface

--- chlidcare-expenditure/usecase/expenditure_ucase.go
+++ chlidcare-expenditure/usecase/expenditure_ucase.go
@@ -26,3 +26,15 @@ func ExpenditureUsecase(
 		txHandler:  th,
 	}
 }
+
+// txHandler is used for handling transaction to begin & commit or rollback
+type txHandler interface {
+	// BeginTx method start transaction (get option from ctx)
+	BeginTx(ctx context.Context, opts interface{}) (tx tx.Context, err error)
+
+	// Commit method commit transaction
+	Commit(tx tx.Context) (err error)
+
+	// Rollback method rollback transaction
+	Rollback(tx tx.Context) (err error)
+}
