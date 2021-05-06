332caebd7e4df499a69eb646885c494cac8e16c8
지농 <jinhong0719@naver.com>
2021-05-06 19:07:40 +0900

    [ADD] <children/repository/mysql/repo_Children.go> Add GetAvailableUUID

--- children/repository/mysql/repo_Children.go
+++ children/repository/mysql/repo_Children.go
@@ -51,3 +51,37 @@ func ChildrenRepository(
 	}
 	return repo
 }
+
+// GetByUUID is implement GetByUUID method of domain.ChildrenRepository interface
+func (cr *childrenRepository) GetByUUID(ctx tx.Context, uuid string) (children domain.Children, err error) {
+	_tx, _ := ctx.Tx().(*sqlx.Tx)
+	_sql, args, _ := squirrel.Select("*").From("children").Where("uuid = ?", uuid).ToSql()
+
+	switch err = _tx.Get(&children, _sql, args...); err {
+	case nil:
+		break
+	case sql.ErrNoRows:
+		err = domain.ErrRowNotExist{RepoErr: errors.Wrap(err, "failed to select children")}
+	default:
+		err = errors.Wrap(err, "select children return unexpected error")
+	}
+	return
+}
+
+// GetAvailableUUID method return available uuid of children table
+func (cr *childrenRepository) GetAvailableUUID(ctx tx.Context) (*string, error) {
+	pa := new(domain.Children)
+
+	for {
+		uuid := pa.GenerateRandomUUID()
+		_, err := cr.GetByUUID(ctx, uuid)
+
+		if err == nil {
+			continue
+		} else if _, ok := err.(domain.ErrRowNotExist); ok {
+			return &uuid, nil
+		} else {
+			return nil, errors.Wrap(err, "failed to GetByUUID")
+		}
+	}
+}
