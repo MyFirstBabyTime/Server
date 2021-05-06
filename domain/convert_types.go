e19fa51e373d28c5680d802325b3dc716535855d
지농 <jinhong0719@naver.com>
2021-05-06 19:06:24 +0900

    [MODIFY] <domain/convert_types.go> Add Time & TimeValue function

--- domain/convert_types.go
+++ domain/convert_types.go
@@ -2,6 +2,8 @@
 
 package domain
 
+import "time"
+
 // String returns a pointer to the string value passed in.
 func String(v string) *string {
 	return &v
@@ -42,4 +44,18 @@ func BoolValue(v *bool) bool {
 		return *v
 	}
 	return false
+}
+
+// Time returns a pointer to the time.Time value passed in.
+func Time(v time.Time) *time.Time {
+	return &v
+}
+
+// TimeValue returns the value of the time.Time pointer passed in or
+// time.Time{} if the pointer is nil.
+func TimeValue(v *time.Time) time.Time {
+	if v != nil {
+		return *v
+	}
+	return time.Time{}
 }
