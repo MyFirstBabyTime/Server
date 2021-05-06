9f45d7afdf0cdb7fb8d96854660218b221e864ae
지농 <jinhong0719@naver.com>
2021-05-06 15:45:15 +0900

    [ADD] <children/repository/mysql/repo_Chlidren.go> Add childrenRepository

--- children/repository/mysql/repo_Children.go
+++ children/repository/mysql/repo_Children.go
@@ -1,1 +1,27 @@
 package mysql
+
+import "github.com/jmoiron/sqlx"
+
+// childrenRepository is implementation of domain.ChildrenRepository using mysql
+type childrenRepository struct {
+	myCfg childrenRepositoryConfig
+
+	db           *sqlx.DB
+	migrator     migrator
+	sqlMsgParser sqlMsgParser
+	validator    validator
+}
+
+// childrenRepositoryConfig is interface get config value for children repository
+type childrenRepositoryConfig interface{}
+
+// sqlMsgParser is interface used for parse sql result message
+type sqlMsgParser interface {
+	EntryDuplicate(msg string) (entry, key string)
+	NoReferencedRow(msg string) (fk string)
+}
+
+// validator is interface used for validating struct value
+type validator interface {
+	ValidateStruct(s interface{}) (err error)
+}
