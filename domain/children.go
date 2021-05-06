b3f1e6dd489b08494c713c9a5f09fb13eded3493
지농 <jinhong0719@naver.com>
2021-05-06 15:37:04 +0900

    [ADD] <domain/children.go> Add usecase & repository interfae

--- domain/children.go
+++ domain/children.go
@@ -1,6 +1,21 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"github.com/MyFirstBabyTime/Server/tx"
+	"mime/multipart"
+	"time"
+)
+
+// ChildrenUsecase is interface about usecase layer using in delivery layer
+type ChildrenUsecase interface {
+	CreateNewChildren(ctx context.Context, c *Children, profile *multipart.FileHeader) (uuid string, err error)
+}
+
+// ChildrenRepository is repository interface about Children model
+type ChildrenRepository interface {
+	Store(ctx tx.Context, c *Children) error
+}
 
 // Children is model represent parent children using in children domain
 type Children struct {
@@ -12,6 +27,7 @@ type Children struct {
 	ProfileUri *string    `db:"profile_uri"`
 }
 
+// Schema return rdbms schema about Children model
 func (_ Children) Schema() string {
 	return `CREATE TABLE children (
 		uuid        CHAR(11) NOT NULL,
@@ -26,6 +42,7 @@ func (_ Children) Schema() string {
 `
 }
 
+// TableName return table name about Children model
 func (_ Children) TableName() string {
 	return "children"
 }
