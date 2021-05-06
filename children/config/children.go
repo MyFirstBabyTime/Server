22f2cf27f96556a70e04bfa3937a3f48ea8c6b76
지농 <jinhong0719@naver.com>
2021-05-06 19:58:27 +0900

    [ADD] <children/config/children.go> Add ChildrenProfileS3Bucket method

--- children/config/children.go
+++ children/config/children.go
@@ -1,14 +1,36 @@
 package config
 
-// App is the application config about syscheck domain
-var App *authConfig
+import "github.com/spf13/viper"
+
+// App is the application config about children domain
+var App *childrenConfig
 
 // init function initialize App global variable
 func init() {
-	App = &authConfig{}
+	App = &childrenConfig{}
 }
 
-// authConfig have config value and implement various interface about auth config
-type authConfig struct {}
+// childrenConfig have config value and implement various interface about children config
+type childrenConfig struct {
+	// childrenProfileS3Bucket represent aws s3 bucket for chlidren profile
+	childrenProfileS3Bucket *string
+}
+
+// default const value about childrenConfig field
+const (
+	defaultChildrenProfileS3Bucket = "first-baby-time"
+)
+
+// ChildrenProfileS3Bucket implement ChildrenProfileS3Bucket of childrenUsecaseConfig
+func (cc *childrenConfig) ChildrenProfileS3Bucket() string {
+	var key = "children.childrenProfileS3Bucket"
+	if cc.childrenProfileS3Bucket == nil {
+		if _, ok := viper.Get(key).(string); !ok {
+			viper.Set(key, defaultChildrenProfileS3Bucket)
+		}
+		cc.childrenProfileS3Bucket = _string(viper.GetString(key))
+	}
+	return *cc.childrenProfileS3Bucket
+}
 
 func _string(s string) *string { return &s }
