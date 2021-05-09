eceb98ff5c1a97becf261089a92eca6e505f89ed
지농 <jinhong0719@naver.com>
2021-05-10 08:52:36 +0900

    [ADD] <children/delivery/children_handler.go> Add NewChildrenHandler

--- children/delivery/children_handler.go
+++ children/delivery/children_handler.go
@@ -3,11 +3,14 @@ package delivery
 import (
 	"github.com/MyFirstBabyTime/Server/domain"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+
+	"github.com/MyFirstBabyTime/Server/domain"
 )
 
 // childrenHandler represent the http handler for children
 type childrenHandler struct {
-	aUsecase   domain.ChildrenUsecase
+	cUsecase   domain.ChildrenUsecase
 	validator  validator
 	jwtHandler jwtHandler
 }
@@ -21,4 +24,15 @@ type jwtHandler interface {
 // validator is interface used for validating struct value
 type validator interface {
 	ValidateStruct(s interface{}) (err error)
+}
+
+// NewChildrenHandler will initialize the children resources endpoint
+func NewChildrenHandler(r *gin.Engine, cu domain.ChildrenUsecase, v validator, jh jwtHandler) {
+	h := &childrenHandler{
+		cUsecase:   cu,
+		validator:  v,
+		jwtHandler: jh,
+	}
+
+	r.POST("parents/uuid/:parent_uuid/children", h.jwtHandler.ParseUUIDFromToken, h.CreateNewChildren)
 }
