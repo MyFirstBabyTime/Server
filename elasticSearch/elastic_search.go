dcc4a610191766b4ab415d0510367b3c07b2c8ec
mspring03 <mspringmspring@naver.com>
2021-05-31 09:25:51 +0900

    [ADD] <elasticSearch/elastic_search.go> import package

---
+++ elasticSearch/elastic_search.go
@@ -0,0 +1,10 @@
+package elasticSearch
+
+import (
+	"context"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"github.com/pkg/errors"
+	"log"
+	"strings"
+)
